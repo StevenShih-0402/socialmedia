88844386dc2c0f13b0925b708b495d69afb3972a
Steven SHIH <stevenjzshih@ibm.com>
2025-03-23 22:49:25 +0800

    Comment Create.

---
+++ src/main/java/com/example/socialmedia/controller/rq/comment/CommentCreateRq.java
@@ -0,0 +1,21 @@
+package com.example.socialmedia.controller.rq.comment;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentCreateRq {
+
+    @NotNull
+    private Integer postId;
+
+    @NotBlank
+    @Size(max = 255)
+    private String content;
+}
